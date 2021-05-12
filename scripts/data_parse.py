a2748a99b685f575e3a4dc3729f5608c532e8812
virati <virati@gmail.com>
2021-05-12 13:40:20 -0400

    Updates to data_parse to output xlsx file

--- scripts/data_parse.py
+++ scripts/data_parse.py
@@ -13,6 +13,8 @@ import openpyxl as opyx
 from openpyxl import Workbook
 from collections import defaultdict
 
+
+
 ''' Auto nested dictionary '''
 def nestdict():
     return defaultdict(nestdict)
@@ -33,3 +35,11 @@ for row in ws.iter_rows(min_row = 4, max_col = 5, max_row=4432):#ws['B4':'B4432'
     if pt != None: dcharts[pt]['Dx'] = row[4].value; #print(pt)
 #%%
 print(str(len(dcharts)) + ' total patients.')
+
+#%%
+import xlsxwriter
+out_frame = pd.DataFrame(dcharts).T
+
+writer = pd.ExcelWriter('pt_list.xlsx', engine='xlsxwriter')
+out_frame.to_excel(writer,sheet_name='Sheet1')
+writer.save()
