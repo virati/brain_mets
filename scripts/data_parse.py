2a23da4389eb1387e011592762f42379da440fdf
virati <virati@gmail.com>
2021-05-17 14:55:44 -0400

    Updates to data_parsing script to generate excel sheet of core 2012-2019 patients

--- scripts/data_parse.py
+++ scripts/data_parse.py
@@ -12,8 +12,10 @@ import pandas as pd
 import openpyxl as opyx
 from openpyxl import Workbook
 from collections import defaultdict
-
-
+import collections
+import datetime
+from operator import getitem
+import string
 
 ''' Auto nested dictionary '''
 def nestdict():
@@ -29,17 +31,60 @@ ws = wb.active
 charts = []
 dcharts = nestdict()
 
-for row in ws.iter_rows(min_row = 4, max_col = 5, max_row=4432):#ws['B4':'B4432']:
+for row in ws.iter_rows(min_row = 4, max_col = 7, max_row=4432):#ws['B4':'B4432']:
     pt = row[1].value
+    #surgeon
     
-    if pt != None: dcharts[pt]['Dx'] = row[4].value; #print(pt)
+    if row[0].value == None:
+        surgeon = active_surgeon
+    else:
+        active_surgeon = row[0].value
+    
+    if pt != None:
+        active_pt = pt
+        dcharts[pt]['EMPI'] = row[2].value
+    else:
+        pt = active_pt
+    dcharts[pt]['Last Name'] = pt.split()[0][:-1]
+    dcharts[pt]['First Name'] = pt.split()[1]
+
+    if row[4].value != None:
+        dcharts[pt]['Dx'] = row[4].value
+    
+    dcharts[pt]['Pathology'] = '?'
+    
+    if dcharts[pt]['First Date']:
+        if row[6].value != None and row[6].value < dcharts[pt]['First Date']:
+            dcharts[pt]['First Date'] = row[6].value
+    else:
+        dcharts[pt]['First Date'] = row[6].value
+    dcharts[pt]['Surgeon'] = active_surgeon
+    dcharts[pt]['Surgery Date'] = []
+    dcharts[pt]['Alk'] = []
+    dcharts[pt]['EGFR'] = []
+
+    if dcharts[pt]['Dates']:
+        dcharts[pt]['Dates'].append(row[6].value)
+    else:
+        dcharts[pt]['Dates'] = [row[6].value]
+    
+        
 #%%
 print(str(len(dcharts)) + ' total patients.')
 
+
+#%%
+#Sort through the dates now
+date_dcharts = {key: val for key,val in dcharts.items() if val['First Date'] < datetime.datetime(2019,12,31,0,0) and val['First Date'] > datetime.datetime(2012,1,1,0,0)}
+
 #%%
 import xlsxwriter
-out_frame = pd.DataFrame(dcharts).T
+#date sorted dictionary
+sorted_dcharts = collections.OrderedDict(sorted(date_dcharts.items(),key=lambda x: getitem(x[1],'First Date')))
 
-writer = pd.ExcelWriter('pt_list.xlsx', engine='xlsxwriter')
+
+out_frame = pd.DataFrame(sorted_dcharts).T
+
+writer = pd.ExcelWriter('../data/pt_list.xlsx', engine='xlsxwriter')
 out_frame.to_excel(writer,sheet_name='Sheet1')
 writer.save()
